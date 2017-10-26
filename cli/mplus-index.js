f5829b5bd32a961141617bce7504129bc91954df
Micah Walter <micah@micahwalter.com>
2017-10-26 16:59:59 -0400

    indexing to es, but this needs to be throttled or use the bulk api

--- cli/mplus-index.js
+++ cli/mplus-index.js
@@ -3,19 +3,22 @@
 var config = require('config');
 var chalk = require('chalk');
 var program = require('commander');
-
+var fs = require('fs');
+var path = require("path");
 var elasticsearch = require('elasticsearch');
 
 const writerConfig = config.get('data');
+const p = "data/json/";
 
 program
     .option('-c, --create <name>', 'Create a new index')
     .option('-d, --drop <name>', 'Delete an index')
+    .option('-r, --reindex <name>', 'Reindex all objects to the specified index')
     .parse(process.argv);
 
 
 var client = new elasticsearch.Client({
-  host: 'localhost:9200',
+    host: 'localhost:9200'
 });
 
 
@@ -44,3 +47,55 @@ if (program.drop) {
 
 }
 
+if (program.reindex) {
+    
+    client.indices.delete({
+	index: program.reindex
+    }).then(function (body) {
+	console.log(chalk.bold.red(JSON.stringify(body)));
+    
+	client.indices.create({
+	    index: program.reindex
+	}).then(function (body) {
+	    console.log(chalk.bold.green(JSON.stringify(body)));
+	    
+	    fs.readdir(p, function (err, files) {
+		if (err) {
+		    throw err;
+		}
+		
+		files.map(function (file) {
+		    return path.join(p, file);
+		}).filter(function (file) {
+		    return fs.statSync(file).isFile();
+		}).forEach(function (file) {
+		    console.log("%s (%s)", file, path.extname(file));
+		    
+		    fs.readFile(file, 'utf-8', function (err, data) {
+			if (err) throw err;
+			
+			client.index({
+			    index: 'micah',
+			    type: 'object',
+			    id: path.basename(file, '.json'),
+			    body: data
+			}).then(function (body) {
+			    console.log(chalk.bold.green(JSON.stringify(body)));
+			}, function (error) {
+			    console.trace(error.message);
+			});
+			
+		    });
+		    
+		});
+	    });
+	    
+	}, function (error) {
+	    console.trace(error.message);
+	});
+	
+    }, function (error) {
+	console.trace(error.message);
+    });
+
+}
