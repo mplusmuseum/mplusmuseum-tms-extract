99bddc78d3e6733850b5afab6c0db7144a59c774
Micah Walter <micah@micahwalter.com>
2017-10-26 15:53:37 -0400

    talk to es

--- cli/mplus-index.js
+++ cli/mplus-index.js
@@ -3,10 +3,44 @@
 var config = require('config');
 var chalk = require('chalk');
 var program = require('commander');
+
 var elasticsearch = require('elasticsearch');
 
 const writerConfig = config.get('data');
 
 program
+    .option('-c, --create <name>', 'Create a new index')
+    .option('-d, --drop <name>', 'Delete an index')
     .parse(process.argv);
+
+
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+});
+
+
+if (program.create) {
+    
+    client.indices.create({
+	index: program.create
+    }).then(function (body) {
+	console.log(chalk.bold.green(JSON.stringify(body)));
+    }, function (error) {
+	console.trace(error.message);
+    });
+
+}
+
+
+if (program.drop) {
+    
+    client.indices.delete({
+	index: program.drop
+    }).then(function (body) {
+	console.log(chalk.bold.red(JSON.stringify(body)));
+    }, function (error) {
+	console.trace(error.message);
+    });
+
+}
 
