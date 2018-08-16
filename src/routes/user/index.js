a275e0b1690a7f1c42b4bb78ef7070f1850c8172
revdancatt <github@revdancatt.com>
2018-08-16 17:03:41 +0100

    Switch languages

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,3 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const langDir = path.join(__dirname, '../../../lang')
+
 exports.settings = (req, res) => {
+  //  Grab the languages
+  const langs = fs.readdirSync(langDir).filter((lang) => {
+    const langSplit = lang.split('.')
+    if (langSplit.length !== 3) return false
+    if (langSplit[0] !== 'strings' || langSplit[2] !== 'json') return false
+    return true
+  }).map((lang) => {
+    const langSplit = lang.split('.')
+    return langSplit[1]
+  })
+  req.templateValues.langs = langs
   return res.render('user/settings', req.templateValues)
+}
+
+exports.setLanguage = (req, res) => {
+  console.log(req.params)
+  return res.redirect('/settings')
 }
