6e2c1cd96f0d3f93572760490cc136e9aa54288e
revdancatt <github@revdancatt.com>
2018-09-13 13:28:05 +0100

    Add new search pages

--- src/routes/search/index.js
+++ src/routes/search/index.js
@@ -1,2 +1,4 @@
-exports.objects = require('./objects.js')
+exports.concepts = require('./concepts.js')
 exports.constituents = require('./constituents.js')
+exports.exhibitions = require('./exhibitions.js')
+exports.objects = require('./objects.js')
