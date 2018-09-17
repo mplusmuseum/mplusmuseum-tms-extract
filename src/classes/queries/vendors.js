922bdde3fbd4efc27fb3e65e1f9fb72f00f45b27
revdancatt <github@revdancatt.com>
2018-09-17 11:22:31 +0100

    Add timeline documentation

--- src/classes/queries/vendors.js
+++ src/classes/queries/vendors.js
@@ -3,7 +3,7 @@ class Queries {
   /**
    * Create a collection of queries
    */
-  constructor() {
+  constructor () {
     this.schema = `query {
   __schema {
     types {
@@ -432,15 +432,27 @@ class Queries {
   }
 }`
 
+    this.timeline = `query {
+  timeline[[]] {
+    id
+    displayDate
+    yearDisplayDate
+    startDate
+    contexts
+    title
+    paragraph
+    imagesObjectId
+    imagesTitle
   }
-
+}`
+  }
   /**
    *
    * @param {string} query The name of the query, needs to match one of those defined in the constructor, i.e. 'schema', 'hello', places'
    * @param {string} filter The filter we want to apply to the query i.e. '(limit: 20)'
    * @returns {string|null} A representation of the query ready to be used if found, or null if not.
    */
-  get(query, filter) {
+  get (query, filter) {
     if (!(query in this)) return null
     return this[query].replace('[[]]', filter)
   }
