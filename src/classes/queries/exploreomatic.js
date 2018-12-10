b22e4ac64fd678ebbceb329e4ad9061011a63845
revdancatt <github@revdancatt.com>
2018-12-10 11:46:26 +0000

    Allow searching of makers by language

--- src/classes/queries/exploreomatic.js
+++ src/classes/queries/exploreomatic.js
@@ -3,7 +3,7 @@ class Queries {
   /**
    * Create a collection of queries
    */
-  constructor() {
+  constructor () {
     this.object = `query {
       object[[]] {
         id
@@ -315,7 +315,7 @@ class Queries {
     }`
 
     this.makertypes = `query {
-      makertypes {
+      makertypes[[]] {
         title
       }
     }`
@@ -377,7 +377,7 @@ class Queries {
     }`
 
     this.exhibitions = `query {
-      exhibitions {
+      exhibitions[[]] {
         id
         title
       }
@@ -448,7 +448,7 @@ class Queries {
    * @param {string} filter The filter we want to apply to the query i.e. '(limit: 20)'
    * @returns {string|null} A representation of the query ready to be used if found, or null if not.
    */
-  get(query, filter) {
+  get (query, filter) {
     if (!(query in this)) return null
     if (!filter) filter = ''
     return this[query].replace('[[]]', filter)
